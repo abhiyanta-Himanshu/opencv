bccd7a63e8ba0919b4dc475daf8fe9e534ea997f
abhiyanta-Himanshu <himanshupal.hp.17@gmail.com>
2024-01-27 17:09:28 +0530

    thresholding

---
+++ 13_thresholding.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('photo/6.jpeg')
+cv.imshow('img',img)
+
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+cv.imshow('gray',gray)
+
+# simple thresholding
+threshold , thresh = cv.threshold(gray,100,255,cv.THRESH_BINARY)
+cv.imshow('simple Threshold', thresh)
+
+# Adaptive threshold
+adaptive_thresh = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,3)
+cv.imshow('adaptive thresh',adaptive_thresh)
+
+cv.waitKey(0)
